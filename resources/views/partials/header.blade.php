c4224bc78f3e84f67439f54132ee15ce59f93e15
Francesco <francesco.savignano96@gmail.com>
2021-01-07 16:53:56 +0100

    header-bottom inizializzato

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -17,7 +17,50 @@
     </div>
     <div id="header-bottom">
         <div class="container">
+            <nav class="navbar">
+                <div class="logo">
+                    <a href="/">
+                        <img src="{{asset('images/dc-logo.png')}}" alt="">
+                    </a>
+                </div>
+                <div class="menu">
+                    <ul>
+                        <li>
+                            <a class="uppercase" href="#">Characters</a>
+                        </li>
+                        <li>
+                            <a class="uppercase active" href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">Movies</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">Tv</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">Games</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">Collectibles</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">Videos</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">Fans</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">News</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">Shop▾</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="search">
 
+                </div>
+            </nav>
         </div>
     </div>
 
