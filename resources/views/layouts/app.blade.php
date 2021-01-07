121cde0e299616fe066108256cf42f7667b9f845
Francesco <francesco.savignano96@gmail.com>
2021-01-07 15:21:48 +0100

    file di layout inizializzato

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+
+    </head>
+    <body>
+
+        <header>
+            <div id="header-top">
+
+            </div>
+            <div id="header-bottom">
+
+            </div>
+        </header>
+
+        @yield('content')
+
+        <footer>
+            <div id="footer-top">
+
+            </div>
+            <div id="footer-bottom">
+
+            </div>
+        </footer>
+    </body>
+</html>
