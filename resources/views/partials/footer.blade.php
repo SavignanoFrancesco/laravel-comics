87ab6f1e79a727c3e0945406c4118185f4fccf39
Francesco <francesco.savignano96@gmail.com>
2021-01-07 19:47:39 +0100

    footer-top impostato

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,10 +1,170 @@
-<div class="container">
-    <footer>
-        <div id="footer-top">
+<footer>
+    <div id="footer-top">
+        <div class="container">
+            <div class="left">
+                <div class="lists-container">
+                    <div class="double-list">
+                        <ul>
+                            <li>
+                                <h3 class="uppercase">DC Comics</h3>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Characters</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Comics</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Movies</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>TV</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Games</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Viedos</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>News</span>
+                                </a>
+                            </li>
+                        </ul>
 
-        </div>
-        <div id="footer-bottom">
+                        <ul>
+                            <li>
+                                <h3 class="uppercase">Shop</h3>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Shop DC</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Shop DC Collectibles</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="single-list">
+                        <ul>
+                            <li>
+                                <h3 class="uppercase">DC</h3>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Terms Of Use</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Privacy Policy (New)</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Add Choises</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Advertising</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Jobs</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Subscriptions</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Talent Workshops</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>CPSC Certificates</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Ratings</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Shop Help</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{Route('contact')}}">
+                                    <span>Contact Us</span>
+                                </a>
+                            </li>
+                    </div>
+                    <div class="single-list">
+                        <ul>
+                            <li>
+                                <h3 class="uppercase">Sites</h3>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>DC</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>MAD Magazine</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>DC Kids</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>DC Universe</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>DC Power Visa</span>
+                                </a>
+                            </li>
+                    </div>
+                </div>
+                <div class="credits">
+                    <span>All Site Content TM and © 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved.</span>
+                    <span><a href="#">Cookies Settings</a></span>
+                </div>
+            </div>
+            <div class="right">
 
+            </div>
         </div>
-    </footer>
-</div>
+    </div>
+    <div id="footer-bottom">
+
+    </div>
+</footer>
