fa6f56feb9f22fd94c880d011e18d97e70e2bf5a
Francesco <francesco.savignano96@gmail.com>
2021-01-07 15:33:28 +0100

    partials scss inizializzati

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,15 @@
 <header>
     <div id="header-top">
-
+        <div class="">
+            <ul>
+                <li>
+                    <a href="#">DC POWER℠ VISA®</a>
+                </li>
+                <li>
+                    <a href="#">Additional DC Sites</a>
+                </li>
+            </ul>
+        </div>
     </div>
     <div id="header-bottom">
 
