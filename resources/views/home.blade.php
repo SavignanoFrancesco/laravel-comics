0ff03134dbb19384aeed5f398c797982ba6031e3
Francesco <francesco.savignano96@gmail.com>
2021-01-07 18:57:45 +0100

    sezione shopping links impostata

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,4 +26,51 @@
             </div>
         </div>
     </div>
+
+    <div id="shopping-links">
+        <div class="container">
+            <ul>
+                <li>
+                    <a href="#">
+                        <div class="imgs">
+                            <img src="https://www.dccomics.com/sites/all/themes/dc_comics_bp/images/buy-comics-digital-comics.png" alt="">
+                        </div>
+                        <h4 class="uppercase">Digital Comics</h4>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="imgs">
+                            <img src="https://www.dccomics.com/sites/all/themes/dc_comics_bp/images/buy-comics-digital-comics.png" alt="">
+                        </div>
+                        <h4 class="uppercase">Digital Comics</h4>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="imgs">
+                            <img src="https://www.dccomics.com/sites/all/themes/dc_comics_bp/images/buy-comics-subscriptions.png" alt="">
+                        </div>
+                        <h4 class="uppercase">Subscription</h4>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="imgs">
+                            <img src="https://www.dccomics.com/sites/all/themes/dc_comics_bp/images/buy-comics-shop-locator.png" alt="">
+                        </div>
+                        <h4 class="uppercase">Comic Shop Locator</h4>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="imgs">
+                            <img src="https://www.dccomics.com/sites/all/themes/dc_comics_bp/images/buy-dc-power-visa.svg" alt="">
+                        </div>
+                        <h4 class="uppercase">DC Power Visa</h4>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
 @endsection
